cb543fca357b13cdec69eb5495b32aac967074e0
Allan <allan.xiao@gmail.com>
2016-01-18 19:02:09 -0500

    not sure...
    Seems sync was incomplete

--- routes/user.js
+++ routes/user.js
@@ -5,17 +5,33 @@ var DAO = require("../helpers/DAO");
 
 var Promise = require("bluebird");
 
+var SCHOOL = "school";
+var MAJOR1 = "major1";
+var MAJOR2 = "major2";
+var MINOR = "minor";
+
+function processPromises (results, promisesToExecute, onResults, onDone, aux) {
+
+    if (promisesToExecute.length > 0) {
+        Promise.all(promisesToExecute).then(function (r2) {
+            processPromises(r2, [], onResults, onDone, aux);
+        });
+    }
+    else {
+        onResults(results, promisesToExecute, aux);
+
+        if (promisesToExecute.length == 0) {
+            onDone();
+        }
+        else
+            processPromises(null, promisesToExecute, onDone, aux)
+    }
+}
 
 router.post('/addEducation', function (req, res, next) {
 
 
     //TODO: Input Validation, including the ones currently added by default
-
-
-    var SCHOOL = "school";
-    var MAJOR1 = "major1";
-    var MAJOR2 = "major2";
-    var MINOR = "minor";
 
     var lookupMap = [
         SCHOOL,
@@ -37,157 +53,81 @@ router.post('/addEducation', function (req, res, next) {
         daoCalls.push(DAO.getDegreeByName(req, req.body.minor))
     }
 
-    var finalValues = {};
+    //Recursive
 
-    var processResult = function (results, lookup, promisesToExecute, final) {
+    var onResults = function(results, newPromises, extras)
+    {
+        var lookup = extras.lookup;
+        var final = extras.final;
 
+        var l_length = lookup.length;
 
-        if (promisesToExecute.length > 0) {
-            Promise.all(promisesToExecute).then(function (r2) {
-                processResult(r2, lookup, [], final);
-            });
-        }
-        else {
+        for (var i = 0; i < results.length; i++) {
+            var result = results[i];
+            var key = lookup[i];
 
-            var l_length = lookup.length;
-
-            for (var i = 0; i < results.length; i++) {
-                var result = results[i];
-                var key = lookup[i];
-
-                console.log("KEY: " + key)
-
-                if (key === SCHOOL) {
-                    if (result === null)
-                        return res.send("School not found")
-                    else
-                        final[key] = result.id;
+            if (key === SCHOOL) {
+                if (result === null)
+                    return res.send("School not found")
+                else
+                    final[key] = result.id;
+            }
+            else if (key === MAJOR1) {
+                if (result === null) {
+                    promisesToExecute.push(
+                        req.dbModels.Degree.create(
+                            {
+                                name: req.body.major1,
+                                lname: req.body.major1.toLowerCase()
+                            }
+                        ));
+                    lookup.push(MAJOR1);
                 }
-                else if (key === MAJOR1) {
-                    console.log("MAJOR1" + JSON.stringify(result))
-
-                    if (result === null) {
-                        promisesToExecute.push(
-                            req.dbModels.Degree.create(
-                                {
-                                    name: req.body.major1,
-                                    lname: req.body.major1.toLowerCase()
-                                }
-                            ));
-                        lookup.push(MAJOR1);
-                    }
-                    else {
-                        final[key] = result.id;
-                    }
-                }
-                else if (key === MAJOR2) {
-                    console.log("MAJOR2" + JSON.stringify(result))
-                    if (result === null) {
-                        promisesToExecute.push(
-                            req.dbModels.Degree.create(
-                                {
-                                    name: req.body.major2,
-                                    lname: req.body.major2.toLowerCase()
-                                }
-                            ));
-                        lookup.push(MAJOR2);
-                    }
-                    else
-                        final[key] = result.id;
+                else {
+                    final[key] = result.id;
                 }
             }
-
-            lookup.splice(0, l_length)
-
-            if (promisesToExecute.length == 0) {
-                res.json(final);
+            else if (key === MAJOR2) {
+                if (result === null) {
+                    promisesToExecute.push(
+                        req.dbModels.Degree.create(
+                            {
+                                name: req.body.major2,
+                                lname: req.body.major2.toLowerCase()
+                            }
+                        ));
+                    lookup.push(MAJOR2);
+                }
+                else
+                    final[key] = result.id;
             }
-            else
-                processResult(null, lookup, promisesToExecute, final)
+            else if (key === MINOR) {
+                if (result === null) {
+                    promisesToExecute.push(
+                        req.dbModels.Degree.create(
+                            {
+                                name: req.body.minor,
+                                lname: req.body.minor.toLowerCase()
+                            }
+                        ));
+                    lookup.push(MINOR);
+                }
+                else
+                    final[key] = result.id;
+            }
         }
+
+        //Remove the processed ones
+        lookup.splice(0, l_length)
     }
 
-    processResult(null, lookupMap, daoCalls, finalValues);
+    var aux = {
+        lookup: lookupMap,
+        final: {}
+    };
 
-    /*
-     Promise.all(daoCalls)
-     .then(function (results) {
-
-     for (var i = 0; i < results.length; i++) {
-     var result = results[i];
-     var key = lookupMap[i];
-
-     if (key === SCHOOL) {
-     if (result === null)
-     return res.send("School not found")
-     else
-     finalValues[key] = result.id;
-     }
-     else if (key === MAJOR) {
-     if (result === null)
-     return res.send("Major not found")
-     else
-     finalValues[key] = result.id;
-     }
-     }
-
-     var lm2 = [];
-     var p2 = []
-     if (results[1] === null)
-
-
-     res.json(data);
-     })*/
+    processPromises(null, daoCalls, onResults, function() {res.json(aux.final)}, aux);
 })
-
-/*
- router.post('/addEducation', function (req, res, next) {
-
- //TODO: Validate
- var values = {};
-
- var doPromise = function (promise, name, saveAs) {
- return promise(req, name)
- .then(function (data) {
- if (data === null)
- return res.send("school not found");
- else
- values[saveAs] = data.id;
- })
- }
-
-
- var params = req.body;
- if (params.school.length < 3) //TODO: Check min max length of all fields pre query
- return res.send("School is required")
-
- DAO.getSchoolByName(req, req.body.school)
- .then(function (data) {
- if (data === null)
- return res.send("school not found");
- else {
- values["school"] = data.id;
-
- DAO.getDegreeByName(req, req.body.major1)
- .then(function (data) {
- if (data === null)
- req.dbModels.Degree
- .create({name: req.body.major1, lname: req.body.major1.toLowerCase()})
- .then(function (model) {
- values["major1"] = model.id;
- res.json(values)
- }
- )
- else {
- values["major1"] = data.id;
- res.json(values)
- }
- }
- );
- }
- })
- });
- */
 
 router.post('/editEducation', function (req, res, next) {
 });
